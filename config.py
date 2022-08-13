cddb4b88abf45323d838526fb81ccd45412fd26c
mfx1021 <111160924+mfx1021@users.noreply.github.com>
2022-08-13 14:01:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 # 公众号配置
 # 公众号appId
-app_id = ""
+app_id = "wx90b40e8cc1b14822"
 # 公众号appSecret
-app_secret = ""
+app_secret = "972aec6b1bf808840c8ca27d56df8dc5"
 # 模板消息id
-template_id = ""
+template_id = "mEMwhR2Ui-XB6adZrvfheh_vStQ7pya5vfIFjjc4d84"
 # 接收公众号消息的微信号
-user = ""
+user = "o-1sX6qTJyguLOldBg5vRoFjdteg"
 
 # 信息配置
 # 所在省份
-province = "广东"
+province = "河北"
 # 所在城市
-city = "广州"
+city = "沧州"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
-birthday = "1997-1-1"
+birthday = "2000-8-2"
 # 在一起的日子，格式同上
-love_date = "2022-1-1"
+love_date = "2022-5-3"
 
